f503bf9b9a109aded6590b2e6318eb13329c14d2
© DzBotz <89877640+DzWangy@users.noreply.github.com>
2021-09-10 02:04:10 +0700

    Update click.js

--- js/click.js
+++ js/click.js
@@ -1,5 +1,5 @@
 function openLink() {
 
-    window.open('https://api.whatsapp.com/send?phone=6283862247572&text=!menu', '_blank')
+    window.open('https://api.whatsapp.com/send?phone=6283862247572&text=5573', '_blank')
 
 }
